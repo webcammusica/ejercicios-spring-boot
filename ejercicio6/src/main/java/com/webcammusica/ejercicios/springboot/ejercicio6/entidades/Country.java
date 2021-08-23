720acd9375baf16277121b57c96cbf3c141e722f
webcammusica <carlos.eduardo.diaz.penuela@gmail.com>
2021-08-23 15:18:54 -0500

    entidad JPA Country.java
    usará la tabla "countries" de la base de datos en mysql.

---
+++ ejercicio6/src/main/java/com/webcammusica/ejercicios/springboot/ejercicio6/entidades/Country.java
@@ -0,0 +1,74 @@
+package com.webcammusica.ejercicios.springboot.ejercicio6.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "countries")
+public class Country extends EntidadAuditable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	@NotNull
+	private String name;
+
+	@Column(nullable = false)
+	@NotNull
+	@Min(1)
+	@Max(2000000000)
+	private Integer population;
+
+	/**
+	 * constructor heredado de EntidadAuditable
+	 */
+	public Country() {
+		super();
+	}
+
+	/**
+	 * constructor
+	 * 
+	 * @param name
+	 * @param population
+	 */
+	public Country(String name, Integer population) {
+		super();
+		this.name = name;
+		this.population = population;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(Integer population) {
+		this.population = population;
+	}
+
+}
