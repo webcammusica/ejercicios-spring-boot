4d4d725647071bbc6a3cdebbed6f6aae5a130677
webcammusica <carlos.eduardo.diaz.penuela@gmail.com>
2021-08-23 14:58:17 -0500

    Create EntidadAuditable.java

---
+++ ejercicio6/src/main/java/com/webcammusica/ejercicios/springboot/ejercicio6/entidades/EntidadAuditable.java
@@ -0,0 +1,71 @@
+package com.webcammusica.ejercicios.springboot.ejercicio6.entidades;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners({ AuditingEntityListener.class })
+@MappedSuperclass
+public class EntidadAuditable {
+
+	@CreatedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
+	private Calendar createdDate;
+
+	@LastModifiedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
+	private Calendar lastModifiedDate;
+
+	@CreatedBy
+	@Column(nullable = false)
+	private String createBy;
+
+	@LastModifiedBy
+	private String lastModifiedBy;
+
+	public Calendar getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Calendar createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Calendar getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Calendar lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	public String getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+
+	public void setLastModifiedBy(String lastModifiedBy) {
+		this.lastModifiedBy = lastModifiedBy;
+	}
+
+	
+}
