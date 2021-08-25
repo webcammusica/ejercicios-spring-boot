75501bd3437b902d5f276181fb60b860b846c29f
webcammusica <carlos.eduardo.diaz.penuela@gmail.com>
2021-08-25 15:51:45 -0500

    Update Country.java
    creación de campos faltantes de fecha en la base de datos y extensión a la clase padre entidad auditable que contiene algunos de los campos como "created_by".

--- ejercicio6/src/main/java/com/webcammusica/ejercicios/springboot/ejercicio6/entidades/Country.java
+++ ejercicio6/src/main/java/com/webcammusica/ejercicios/springboot/ejercicio6/entidades/Country.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "countries")
-public class Country {
+public class Country extends EntidadAuditable{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
